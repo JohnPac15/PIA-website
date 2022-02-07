e8541ed7178b538f9c2ce645697d4b18cb2797b3
Robert C <robert.clarady@gmail.com>
2022-02-07 08:30:13 -0600

    built user access functions

--- auth/_helpers.js
+++ auth/_helpers.js
@@ -5,7 +5,9 @@ function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
 
-function createUser (req) {
+function createUser(req, res) {
+  return handleErrors(req)
+  .then(() => {
     const salt = bcrypt.genSaltSync();
     const hash = bcrypt.hashSync(req.body.password, salt);
     return knex('users')
@@ -14,6 +16,10 @@ function createUser (req) {
       password: hash
     })
     .returning('*');
+  })
+  .catch((err) => {
+    res.status(400).json({status: err.message});
+  });
 }
 
 function loginRequired(req, res, next) {
@@ -21,8 +27,46 @@ function loginRequired(req, res, next) {
     return next();
 }
 
+function adminRequired(req, res, next) {
+    if (!req.user) res.status(401).json({status: 'Please log in'});
+    return knex('users').where({username: req.user.username}).first()
+    .then((user) => {
+      if (!user.admin) res.status(401).json({status: 'You are not authorized'});
+      return next();
+    })
+    .catch((err) => {
+      res.status(500).json({status: 'Something bad happened'});
+    });
+}
+
+function loginRedirect(req, res, next) {
+    if (req.user) return res.status(401).json(
+      {status: 'You are already logged in'});
+    return next();
+}
+
+function handleErrors(req) {
+  return new Promise((resolve, reject) => {
+    if (req.body.username.length < 6) {
+      reject({
+        message: 'Username must be longer than 6 characters'
+      });
+    }
+    else if (req.body.password.length < 6) {
+      reject({
+        message: 'Password must be longer than 6 characters'
+      });
+    } else {
+      resolve();
+    }
+  });
+}
+
 module.exports = {
   comparePass,
   createUser,
-  loginRequired
+  loginRequired,
+  adminRequired,
+  loginRedirect,
+  handleErrors
 };
