851474d7d943846bc5be3f04f41b5ed30617648d
Robert C <robert.clarady@gmail.com>
2022-02-07 12:50:33 -0600

    updating User model and routes

--- controllers/api/policy-owner.js
+++ controllers/api/policy-owner.js
@@ -1,8 +1,35 @@
 const router = require('express').Router();
+const { adminRequired } = require('../../auth/_helpers');
 const {User, Policies, Homeowners, Auto} = require('../../models');
 
 router.get('/', (req, res) => {
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
+  .then(dbPolicyOwnerData => res.json(dbPolicyOwnerData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
+
+router.get('/policyowners', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] },
+    where: { policyOwner = true },
+  })
+  .then(dbPolicyOwnerData => res.json(dbPolicyOwnerData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
+
+router.get('/admins', adminRequired, (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] },
+    where: { admin = true },
+  })
   .then(dbPolicyOwnerData => res.json(dbPolicyOwnerData))
   .catch((err) => {
     console.log(err);
@@ -23,5 +50,28 @@ router.post('/', (req, res) => {
     res.status(500).json(err);
   });
 })
+
+router.put('/:username', adminRequired, (req, res) => {
+  User.update(
+    {
+      admin: req.body.admin
+    },
+    {
+    where: {
+      username: req.params.username
+      }
+    })
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this username' });
+      return;
+  }
+  res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -26,6 +26,7 @@ User.init({
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   password: {
     type: DataTypes.STRING,
@@ -34,7 +35,7 @@ User.init({
       len: [4],
     },
   },
-  PolicyOwner: {
+  policyOwner: {
     type: DataTypes.BOOLEAN,
     allowNull: false,
     defaultValue: true,
