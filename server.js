5b7e07407f0feb64b0df156d8c24c0fe8c9ec676
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-02-08 17:02:23 -0600

    added a login page and modified the main/homepage

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('views/images')); 
 
 app.use(routes);
 
