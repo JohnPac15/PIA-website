758f76a5b4b99157d737d45cd6bebd9e5fb43aa5
Robert C <robert.clarady@gmail.com>
2022-02-08 19:40:32 -0600

    fixed merged error

--- auth/_helpers.js
+++ auth/_helpers.js
@@ -31,41 +31,6 @@ function loginRedirect(req, res, next) {
     return next();
 }
 
-function adminRequired(req, res, next) {
-    if (!req.user) res.status(401).json({status: 'Please log in'});
-    return knex('users').where({username: req.user.username}).first()
-    .then((user) => {
-      if (!user.admin) res.status(401).json({status: 'You are not authorized'});
-      return next();
-    })
-    .catch((err) => {
-      res.status(500).json({status: 'Something bad happened'});
-    });
-}
-
-function loginRedirect(req, res, next) {
-    if (req.user) return res.status(401).json(
-      {status: 'You are already logged in'});
-    return next();
-}
-
-function handleErrors(req) {
-  return new Promise((resolve, reject) => {
-    if (req.body.username.length < 6) {
-      reject({
-        message: 'Username must be longer than 6 characters'
-      });
-    }
-    else if (req.body.password.length < 6) {
-      reject({
-        message: 'Password must be longer than 6 characters'
-      });
-    } else {
-      resolve();
-    }
-  });
-}
-
 module.exports = {
   loginRequired,
   adminRequired,
